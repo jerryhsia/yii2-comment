a77a91fef8194d15b336e8aca62db80b006ff93f
Jerry <jerry9916@qq.com>
2015-06-30 18:18:10 +0800

    Add methods for CommentService

--- comment/CommentService.php
+++ comment/CommentService.php
@@ -7,8 +7,31 @@ class CommentService extends Component
 {
     public $commentClass = 'jerryhsia\comment\Comment';
 
-    public function save()
+    /**
+     * @param array $attributes
+     * @return \jerryhsia\comment\Comment
+     */
+    public function save(array $attributes = [])
     {
+        $model = new $this->commentClass();
 
+        $model->setAttributes($attributes, false);
+
+        return $model->save();
+    }
+
+    public function search(array $filter = [])
+    {
+        $query = ($this->commentClass)::find();
+
+        $eqFields = ['owner_group_id', 'owner_id'];
+
+        foreach ($eqFields as $field) {
+            if (isset($filters[$field])) {
+                $query->andFilterWhere([$field => $filters[$field]]);
+            }
+        }
+
+        return $query;
     }
 }
